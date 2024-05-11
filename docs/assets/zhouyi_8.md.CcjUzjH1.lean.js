8900e39020c997620e0971a44783f3df15bee0d5
LarryZhu-dev <larryzhu@foxmail.com>
2024-05-11 17:07:35 +0800

    move to docs dir

---
+++ docs/assets/zhouyi_8.md.CcjUzjH1.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a2 as _}from"./chunks/framework.SaK6vuij.js";const m=JSON.parse('{"title":"第八卦 比 水地比 坎上坤下","description":"","frontmatter":{},"headers":[],"relativePath":"zhouyi_8.md","filePath":"zhouyi_8.md"}'),o={name:"zhouyi_8.md"},p=_("",10),r=[p];function s(c,i,n,d,h,l){return a(),t("div",null,r)}const f=e(o,[["render",s]]);export{m as __pageData,f as default};
---
+++ docs/assets/zhouyi_9.md.BdTHzxlk.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a2 as _}from"./chunks/framework.SaK6vuij.js";const m=JSON.parse('{"title":"第九卦 小畜 风天小畜 巽上乾下","description":"","frontmatter":{},"headers":[],"relativePath":"zhouyi_9.md","filePath":"zhouyi_9.md"}'),o={name:"zhouyi_9.md"},p=_("",10),r=[p];function s(c,i,n,d,h,l){return a(),t("div",null,r)}const f=e(o,[["render",s]]);export{m as __pageData,f as default};
